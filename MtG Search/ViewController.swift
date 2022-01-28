4e61050323af201d9915340fd51d44d340b83fa1
Arkow0 <50901436+Arkow0@users.noreply.github.com>
2022-01-28 20:08:58 +0100

    Initial Commit

---
+++ MtG Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MtG Search
+//
+//  Created by Arkow on 28/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
