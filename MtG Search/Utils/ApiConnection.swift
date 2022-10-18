4f7d4f69c2ff361fe811e206da43178e6c29c512
Arkow0 <50901436+Arkow0@users.noreply.github.com>
2022-10-18 20:51:49 +0200

    Uncomment APiClient

--- MtG Search/Utils/ApiConnection.swift
+++ MtG Search/Utils/ApiConnection.swift
@@ -4,38 +4,38 @@
 //
 //  Created by Arkow on 10/08/2022.
 //
-//
-//import Foundation
-//import UIKit
-//
-//
-//
-//final class ApiConnction {
-//
-//    // MARK: - Properties
-//
-//    private let session: URLSession = .shared
-//   // private let baseUrl: String = Constants.baseURL
-//   //  let modyfiQuery = query.replacingOccurrences(of: " ", with: "+")
-//    static let shared: ApiConnction = .init()
-//
-//    // MARK: - Methods
-//
-//    func getDate(for query: String) {
-//        let modyfiQuery = query.replacingOccurrences(of: " ", with: "+")
-//        guard let cardUrl = URL(string: "https://api.scryfall.com/cards/search?q=\(modyfiQuery)&format=json") else {return}
-//
-//
-//        let task = session.dataTask(with: cardUrl) { data, response, error in
-//            if let data = data {
-//                let decoder = JSONDecoder()
-//                if let dataAll = try? decoder.decode(DataAll.self, from: data) {
-////                    self.cardTab = dataAll?.data ?? []
-////                    self.collectionView.reloadData()
-//                }
-//            }
-//        }
-//        task.resume()
-//    }
-//
-//}
+
+import Foundation
+import UIKit
+
+
+
+final class ApiConnction {
+
+    // MARK: - Properties
+
+    private let session: URLSession = .shared
+   // private let baseUrl: String = Constants.baseURL
+   //  let modyfiQuery = query.replacingOccurrences(of: " ", with: "+")
+    static let shared: ApiConnction = .init()
+
+    // MARK: - Methods
+
+    func getDate(for query: String) {
+        let modyfiQuery = query.replacingOccurrences(of: " ", with: "+")
+        guard let cardUrl = URL(string: "https://api.scryfall.com/cards/search?q=\(modyfiQuery)&format=json") else {return}
+
+
+        let task = session.dataTask(with: cardUrl) { data, response, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                if let dataAll = try? decoder.decode(DataAll.self, from: data) {
+//                    self.cardTab = dataAll?.data ?? []
+//                    self.collectionView.reloadData()
+                }
+            }
+        }
+        task.resume()
+    }
+
+}
